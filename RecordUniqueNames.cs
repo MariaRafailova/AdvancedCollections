3f3dc96299e18b1f4ae159d98fcc6224ed27d4ca
MariaRafailova <mrafailova@gmail.com>
2017-03-05 17:40:48 +0200

    Add files via upload

---
+++ RecordUniqueNames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    public class RecordUniqueNames
+    {
+        public static void Main()
+        {
+            var names = new HashSet<string>();
+            var n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                var name = Console.ReadLine();
+                names.Add(name);
+            }
+            
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
